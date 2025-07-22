a5b1a92e1f247248a2cfc1b0fb29f7ac44ec2e37
de Breyne <111784301+helio75011@users.noreply.github.com>
2025-07-22 23:07:53 +0200

    detect player X or O

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,39 @@
    function handleClick(index) {
     if (board[index] === null) {
         board[index] = currentPlayer;
+
+        const winner = checkWinner(board);
+        if (winner) {
+          alert(`Le joueur ${winner} a gagné !`);
+          return; // on arrête la partie ici
+        }
+
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     }
    }
+
+   function checkWinner(board) {
+    const winningCombos = [
+      [0, 1, 2], // lignes
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6], // colonnes
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8], // diagonales
+      [2, 4, 6]
+    ];
+
+    for (const [a, b, c] of winningCombos) {
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a]; // 'X' ou 'O'
+      }
+    }
+
+    return null;
+   }
+
+
 </script>
 
 <style>
